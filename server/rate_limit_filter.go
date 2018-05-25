298cd021abb81dfccf8b78bc19c56137b0b19cc5
wanghongfei <brucewhf@gmail.com>
2018-05-25 15:35:30 +0800

    提升在无QPS限制时的性能

--- server/rate_limit_filter.go
+++ server/rate_limit_filter.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wanghongfei/gogate/throttle"
 )
 
+// 控制QPS的前置过虑器
 func RateLimitPreFilter(s *Server, ctx *fasthttp.RequestCtx, newRequest *fasthttp.Request) bool {
 	// 取出router结果
 	ctxVal := ctx.UserValue(ROUTE_INFO)
@@ -15,17 +16,19 @@ func RateLimitPreFilter(s *Server, ctx *fasthttp.RequestCtx, newRequest *fasthtt
 
 	// 取出对应service的限速器
 	info := ctxVal.(*ServiceInfo)
-	rlVal, ok := s.rateLimiterMap.Load(info.Id)
-	if !ok {
+	if 0 == info.Qps {
 		// 如果没有说明不需要限速
 		log4go.Debug("no limiter for service %s", info.Id)
 		return true
 	}
 
+	// 取出限速器
+	rlVal, _ := s.rateLimiterMap.Load(info.Id)
 	rl := rlVal.(*throttle.RateLimiter)
+
 	pass := rl.TryAcquire()
 	if !pass {
-		// 没匹配到
+		// token不足
 		NewResponse(ctx.UserValue(REQUEST_PATH).(string), "reach QPS limitation").Send(ctx)
 		log4go.Info("drop request for %s due to rate limitation", info.Id)
 	}
