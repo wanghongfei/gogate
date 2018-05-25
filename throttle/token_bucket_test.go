8ebe2dd28fe1b90808ebbd70849c971927398127
wanghongfei <brucewhf@gmail.com>
2018-05-25 11:25:14 +0800

    fix unit test

--- throttle/token_bucket_test.go
+++ throttle/token_bucket_test.go
@@ -14,18 +14,32 @@ func TestNewRateLimiter(t *testing.T) {
 
 func TestRateLimiter_Acquire(t *testing.T) {
 	rl := NewRateLimiter(1)
+
+	count := 0
 	for {
 		rl.Acquire()
 		fmt.Println(time.Now())
+
+		count++
+		if count >= 10 {
+			break
+		}
 	}
 }
 
 func TestRateLimiter_TryAcquire(t *testing.T) {
+	count := 0
+
 	rl := NewRateLimiter(1)
 	for {
 		pass := rl.TryAcquire()
 		if pass {
 			fmt.Println(time.Now())
+		}
+
+		count++
+		if count >= 10 {
+			break
 		}
 	}
 
